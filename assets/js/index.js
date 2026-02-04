fe27baaec78dfc248c10f3b6ac4cb2c4740a5755
BilousOleg <olezhka82802@gmail.com>
2026-02-04 20:22:38 +0200

    Add first-exericse solution

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// Приклад використання linkedList
+
+const list1 = new LinkedList(1, 2, 3);
+list1.printList();
+list1.append(4);
+list1.printList();
+list1.deleteItem(2);
+list1.printList();
+list1.addNthElement(2, 1);
+list1.printList();
