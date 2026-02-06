feb086757205fc5d25ba1318cfe95b2a1c1addb5
Bilous Oleg <olezhka82802@gmail.com>
2026-02-06 21:06:55 +0200

    Add third exercise solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,15 @@ try {
     null
   );
   console.log(structure1);
+  // Приклад застосування ітератора:
+  const [firstValue, secondValue] = structure1; // Деструктуроване присвоєння
+  console.log('Destructurised assignment:', firstValue, secondValue);
+
+  console.log('Destructuring:', ...structure1);
+
+  for (const element of structure1) {
+    console.log('Value:', element);
+  }
 } catch (error) {
   console.log(error);
 }
